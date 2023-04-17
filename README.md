ea8b5bbcab0fffe3d3f3c0549be4f985900ed6c6
seldacetin <64745949+seldacetin@users.noreply.github.com>
2023-04-17 11:44:17 +0300

    MLP_modelling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ann modelling
+#  modelling
+
+# Diabetes veri kümesindeki hastaların şeker hastası olup olmadığını MLP(Multi-Layer Perceptron) modelleme ile öğrenme.
--- ann.py
+++ ann.py
@@ -1,22 +1,23 @@
 from keras.models import Sequential
 from keras.layers import Dense
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
-dataset = pd.read_csv("diabetes.csv")
-datalar = dataset.iloc[:, 0:10]
-etiketler = dataset
-
-dataset.head()
+np.random.seed(9)
+dataset = np.genfromtxt('diabetes.csv', delimiter=',', skip_header=1)
+data = dataset[:, 0:10]
+etiketler = dataset[:,8]
 
 model = Sequential()
 
-model.add(Dense(25, input_dim = 8, activation='relu'))
-model.add(Dense(50, activation='tanh'))
+model.add(Dense(25, input_dim = 9, activation='relu'))
+model.add(Dense(50, activation='sigmoid'))
 model.add(Dense(1, activation='sigmoid'))
 
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-dataset = model.fit(datalar, etiketler, epochs=500, batch_size=10)
-basarim=model.evaluate(datalar,etiketler)
+model.compile(optimizer='adam', loss='mae', metrics=['accuracy'])
+model.fit(data, etiketler, epochs=100, batch_size=5)
+basarim=model.evaluate(data, etiketler)
 
 print("\n %s : %.2f%%" % (model.metrics_names[1], basarim[1]*100)) 
